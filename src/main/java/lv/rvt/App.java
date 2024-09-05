52df6d2a7a4142caa47d1cafa2ab17791f81bfe7
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-05 09:50:40 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,8 +9,15 @@ public class App
         Scanner scanner = new Scanner(System.in);
         
         // Mes gribam uzzinat no lietotaja vārdu, uzvardu un gruppu
-
+        System.out.println("Uzrakstiet savu vārdu: ");
+        String name = scanner.nextLine();
+        System.out.println("Uzrakstiet savu uzvārdu: ");
+        String lastname = scanner.nextLine( );
+        System.out.println("Uzrakstiet savu gruppu: ");
+        String gruppa = scanner.nextLine( );
+    
         // Un begias gribam visu printet kopa viena rinda izmantojot string savienošanu
-
+        System.out.println(name +" "+ lastname +" "+ gruppa);
+        
     }
 }
