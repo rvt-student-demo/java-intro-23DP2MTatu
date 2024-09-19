07524358ec43b4613523e83a72d0aa6b9f419d42
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-19 10:23:06 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,30 +6,21 @@ public class App
     public static void main(String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        int gift = Integer.valueOf(scanner.nextLine());
-        if (gift < 5000) {
-            System.out.println("No tax");
+        while (true) {
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number > 0) {
+                System.out.println(number*number);
+                continue;
+            }
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            }
+            if (number == 0) {
+                break;
+            }
         }
-        else if (gift < 25000) {
-            double tax = (100+(gift-5000)*0.08);
-            System.out.println("Tax: "+tax);
-        }
-        else if (gift < 55000) {
-            double tax = (1700+(gift-25000)*0.1);
-            System.out.println("Tax: "+tax);
-        }
-        else if (gift < 200000) {
-            double tax = (4700+(gift-55000)*0.12);
-            System.out.println("Tax: "+tax);
-        }
-        else if (gift < 1000000) {
-            double tax = (22100+(gift-200000)*0.15);
-            System.out.println("Tax: "+tax);
-        }
-        else {
-            double tax = (142100+(gift-1000000)*0.17);
-            System.out.println("Tax: "+tax);
-        }
+        
     }
 }
