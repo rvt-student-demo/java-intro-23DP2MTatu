7bc1406d3f3611776e6ee4efd3b7126539ea4527
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-19 08:23:23 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,16 +6,30 @@ public class App
     public static void main(String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Jūsu vārds: ");
-        String name = scanner.nextLine();
-        
-        System.out.println("Jūsu Uzvārds: ");
-        String lastname = scanner.nextLine();
-        
-        System.out.println("Jūsu Grupa: ");
-        String grupa = scanner.nextLine();
-        
-        System.out.println("Vārds: " + name + " Uzvārds: " + lastname + " grupa: " + grupa);
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scanner.nextLine());
+        if (gift < 5000) {
+            System.out.println("No tax");
+        }
+        else if (gift < 25000) {
+            double tax = (100+(gift-5000)*0.08);
+            System.out.println("Tax: "+tax);
+        }
+        else if (gift < 55000) {
+            double tax = (1700+(gift-25000)*0.1);
+            System.out.println("Tax: "+tax);
+        }
+        else if (gift < 200000) {
+            double tax = (4700+(gift-55000)*0.12);
+            System.out.println("Tax: "+tax);
+        }
+        else if (gift < 1000000) {
+            double tax = (22100+(gift-200000)*0.15);
+            System.out.println("Tax: "+tax);
+        }
+        else {
+            double tax = (142100+(gift-1000000)*0.17);
+            System.out.println("Tax: "+tax);
+        }
     }
 }
