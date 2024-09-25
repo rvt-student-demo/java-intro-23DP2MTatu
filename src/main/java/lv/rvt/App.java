3ccbdb2369e9a61919ec18861f4b2f5c02cef97d
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-25 15:16:15 +0000

    Average of positive numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,19 +7,27 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         int NumberNumber = 0;
-        int Summa = 0;
+        float Summa = 0;
         while (true) {
             System.out.println("Give a number: ");
             int number = Integer.valueOf(scanner.nextLine());
-            if (number != 0) {
+            if (number > 0) {
                 NumberNumber += 1;
                 Summa = Summa + number;
+                continue;
+            }
+            if (number < 0) {
                 continue;
             }
             if (number == 0) {
                 break;
             }
         }
-    System.out.println("Average of the numbers: " + Summa/NumberNumber);
+    if (NumberNumber == 0 ){
+        System.out.println("Cannot calculate the average");
+    }
+    else{
+        System.out.println(Summa/NumberNumber);
+    }
     }
  }
