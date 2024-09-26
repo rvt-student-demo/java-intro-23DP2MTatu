e3931ac1176aa0f6ed75baab59d1f950b6b64b8e
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-26 08:39:12 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,28 +6,9 @@ public class App
     public static void main(String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int NumberNumber = 0;
-        float Summa = 0;
-        while (true) {
-            System.out.println("Give a number: ");
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number > 0) {
-                NumberNumber += 1;
-                Summa = Summa + number;
-                continue;
-            }
-            if (number < 0) {
-                continue;
-            }
-            if (number == 0) {
-                break;
-            }
+        int i = Integer.valueOf(scanner.nextLine());
+        for(i = i; i <= 100; i++){
+            System.out.println(i);
         }
-    if (NumberNumber == 0 ){
-        System.out.println("Cannot calculate the average");
     }
-    else{
-        System.out.println(Summa/NumberNumber);
-    }
-    }
- }
+}
