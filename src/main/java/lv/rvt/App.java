7e6ac7b7b675f3b31156a68004e97c40caf56b2c
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-26 10:16:32 +0000

    Even and odd numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,17 +5,33 @@ public class App
 {
     public static void main(String[] args )
     {
+        int sum = 0;
+        int Num = 0; 
+        int Even = 0;
+        int Odd = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number: ");
-        int facN = Integer.valueOf(scanner.nextLine());
-        int fac = 1;
-        if (facN == 0){
-            System.out.println(fac);
-        }
-        else{
-            for(int i = 1; i < facN+1; i++){
-                fac = fac * i;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1){
+                System.out.println("Thx! Bye!");
+                System.out.println("Sum: " + sum);
+                System.out.println("Numbers: " +Num);
+                System.out.println("Average: " + (double) sum/Num);
+                System.out.println("Even: " + Even);
+                System.out.println("Odd: " + Odd);
+                break;
             }
-        }   System.out.println("Factorial: " + fac);
+            else {
+                if (number % 2 == 0) {
+                    Even++;
+                }
+                else {
+                    Odd++;
+                }
+                sum += number;
+                Num++;
+            }
+        }
     }
-}
+} 
