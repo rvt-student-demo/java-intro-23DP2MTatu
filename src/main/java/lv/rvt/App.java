e5d9ee4378637fa3bf348f03bd07f5986bb140a7
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-09-26 09:28:02 +0000

    Factorial

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,9 +6,16 @@ public class App
     public static void main(String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int i = Integer.valueOf(scanner.nextLine());
-        for(i = i; i <= 100; i++){
-            System.out.println(i);
+        System.out.println("Give a number: ");
+        int facN = Integer.valueOf(scanner.nextLine());
+        int fac = 1;
+        if (facN == 0){
+            System.out.println(fac);
         }
+        else{
+            for(int i = 1; i < facN+1; i++){
+                fac = fac * i;
+            }
+        }   System.out.println("Factorial: " + fac);
     }
 }
