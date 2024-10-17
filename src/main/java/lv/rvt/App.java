fa54ddf988442e307705f4eeac3aa5d638afb9b2
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-10-17 08:56:37 +0000

    Exercise 3A — Improved Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,27 +10,39 @@ public class App
         Random rand = new Random();
         
         int win = 0;
-
         System.out.print("I am thinking of a number from 1 to 10."
             +"\nYou must guess what it is in three tries."
             +"\nEnter a guess: ");
         int number = rand.nextInt(10)+1;
         for(int i = 0; i < 3;i++) {
             int userNumber = scanner.nextInt();
-            if (userNumber == number) {
-                System.out.println("RIGHT!");
+            int userNumberTemp = userNumber-number;
+            if(userNumberTemp < 0){
+                userNumberTemp = userNumberTemp * -1;
+            }
+            if(userNumber == number){
                 win = 1;
+                System.out.println("RIGHT!");
                 break;
-            } else {
-                System.out.println("wrong");
+            }
+            switch (userNumberTemp) {
+                case 1:
+                    System.out.println("hot");
+                    break;
+                case 2:
+                    System.out.println("warm");
+                    break;
+                default:
+                    System.out.println("cold");
+                    break;
             }
         }
         if(win == 1){
             System.out.println("You have won the game.");
         }
         else{
-            System.out.println("The correct number was " +number+"."
-                +"\nYou have lost the game.");
+            System.out.println("The correct number was "+number+".");
+            System.out.println("You have lost the game.");
         }
     }
 } 
