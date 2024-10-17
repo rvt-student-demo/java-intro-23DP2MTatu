81f35da0cb9dadb1828ecfedea0978df6dc087dd
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-10-17 08:39:41 +0000

    Exercise 2 — Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,36 @@
 package lv.rvt;
 import java.util.*;
 
+import javax.management.relation.Role;
+
 public class App 
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> number = new ArrayList<>();
-        int index = 0;
-        int summa = 0;
-        while (true) {
-            number.add(scanner.nextInt());
-            if (number.get(index) == 0){
-                number.remove(index);
-                System.out.println(number);
-                System.out.println(number.get(1)+number.get(2));
-                System.out.println(summa);
+        Random rand = new Random();
+        
+        int win = 0;
+
+        System.out.print("I am thinking of a number from 1 to 10."
+            +"\nYou must guess what it is in three tries."
+            +"\nEnter a guess: ");
+        int number = rand.nextInt(10)+1;
+        for(int i = 0; i < 3;i++) {
+            int userNumber = scanner.nextInt();
+            if (userNumber == number) {
+                System.out.println("RIGHT!");
+                win = 1;
                 break;
+            } else {
+                System.out.println("wrong");
             }
-            summa = summa + number.get(index);
-            index++;
+        }
+        if(win == 1){
+            System.out.println("You have won the game.");
+        }
+        else{
+            System.out.println("The correct number was " +number+"."
+                +"\nYou have lost the game.");
         }
     }
 } 
