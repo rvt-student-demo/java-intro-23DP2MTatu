3bf7af7772228ed275926fa45cacc588fdb33f1f
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2024-12-05 14:00:53 +0000

    PersonManager (Part 2)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,24 +2,46 @@ package lv.rvt;
 import java.io.BufferedReader;
 import java.util.*;
 public class App {
-    public static void main(String[] args) {
-        PaymentTerminal unicafeExactum = new PaymentTerminal();
-        System.out.println(unicafeExactum);
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean isProgrammWork = true;
+        
+        while (isProgrammWork) {
+            
+            System.out.println();
+            System.out.println("Welcome to person manager, type \"help\" to see available commands.");
+            
+            String userInput = scanner.nextLine().toLowerCase();
+            System.out.println();
 
-        PaymentCard annesCard = new PaymentCard(2);
+            switch (userInput) {
+            case "exit":
+                isProgrammWork = false;
+                System.out.println("Thanks, bye bye!");
+                break;
+            case "add":
+                System.out.println(
+                "Ievadiet personu pēc parauga: \n" +
+                "{name} {age} {weight} {height}");;
 
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-
-        unicafeExactum.addMoneyToCard(annesCard, 100);
-
-        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        System.out.println(unicafeExactum);
+                PersonManager.addPerson(scanner.nextLine());
+                break;
+            case "show":
+                PersonManager.showPersonList();
+                break;
+            case "help":
+                System.out.println(
+                "add: add person to person list \n" +
+                "show: show person list \n" +
+                "help: show help menu \n" +
+                "exit: end programm"
+                );
+                break;
+            default:
+                System.out.println("unknown command");
+                break;
+           }
+        }
     }
 }
