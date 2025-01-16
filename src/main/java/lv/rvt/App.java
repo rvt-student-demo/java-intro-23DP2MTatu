bae569aa3f6a12afedce678faca4cfa57276aee9
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2025-01-16 09:50:32 +0000

    Person and subclasses (5 parts) - Part 3: Student's toString

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,8 +7,7 @@ public class App {
     public static void main(String[] args) throws Exception{
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
         System.out.println(ollie);
-        System.out.println("Study credits " + ollie.credits());
         ollie.study();
-        System.out.println("Study credits "+ ollie.credits());
+        System.out.println(ollie);
     }
 }
--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -15,4 +15,9 @@ public class Student extends Person{
     public int credits() {
         return this.credits;
     }
+
+    @Override 
+    public String toString() {
+        return super.toString() + "\n" + "Study credits "+ credits();
+    }
 }
