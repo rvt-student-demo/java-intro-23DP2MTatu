852c54b9f7e9fcc53ba5ea8cb6a26a09c049dbba
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2025-01-16 10:02:11 +0000

    Person and subclasses (5 parts) - Part 4: Teacher

---
+++ src/main/java/lv/rvt/Teacher.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+
+public class Teacher extends Person{
+
+    private int salary;
+
+    Teacher(String name, String adress, int salary) {
+        super(name, adress);
+        this.salary = salary;
+    }
+
+    public int salary() {
+        return this.salary;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\n" + "salary " + salary() + " euro/month";
+    }
+}
