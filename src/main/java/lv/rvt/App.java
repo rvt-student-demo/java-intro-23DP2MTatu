81e5250c054b70c6f93c14fa09a07ccfc7c9d96f
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2025-01-29 11:37:33 +0000

    Warehousing (7 parts) - Part 1: Product warehouse, step 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,20 +1,11 @@
 package lv.rvt;
-import java.io.BufferedReader;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<Person> persons = new ArrayList<Person>();
-        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
-    
-        printPersons(persons);
-    }
-
-    public static void printPersons(ArrayList<Person> persons) {
-        for (Person person : persons) {
-            System.out.println(person);
-        }
-    }
+        ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+        juice.addToWarehouse(1000.0);
+        juice.takeFromWarehouse(11.3);
+        System.out.println(juice.getName()); // Juice
+        System.out.println(juice);           // balance = 988.7, space left 11.3
+            }
 }
