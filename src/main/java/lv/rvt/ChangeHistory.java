b80e2bb022325502894d7e82cf2b957d3a1c5ada
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2025-01-29 12:14:54 +0000

    Warehousing (7 parts) - Part 4: Change History, step 2

--- src/main/java/lv/rvt/ChangeHistory.java
+++ src/main/java/lv/rvt/ChangeHistory.java
@@ -2,6 +2,7 @@ package lv.rvt;
 
 import java.util.ArrayList;
 
+
 public class ChangeHistory {
     private ArrayList<Double> history;
     
@@ -18,5 +19,48 @@ public class ChangeHistory {
 
     public String toString() {
         return history.toString();
+    }
+
+    public double maxValue() {
+        if (history == null) {
+            return 0;
+        }
+        else {
+            double max = Double.NEGATIVE_INFINITY;
+            for (Double double1 : history) {
+                if (max < double1) {
+                    max = double1;
+                }
+            }
+            return max;
+        }
+    }
+
+    public double minValue() {
+        if (history == null) {
+            return 0;
+        }
+        else {
+            double min = Double.POSITIVE_INFINITY;
+            for (Double double1 : history) {
+                if (min > double1) {
+                    min = double1;
+                }
+            }
+            return min;
+        }
+    }
+
+    public double average() {
+        if (history == null) {
+            return 0;
+        }
+        else {
+            double avg = 0;
+            for (Double double1 : history) {
+                avg = avg + double1;
+            }
+            return avg/history.size();
+        }
     }
 }
