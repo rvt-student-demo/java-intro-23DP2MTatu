1c9fc4b42b5eea94f11771315e6fb65f7a05969a
23DP2MTatu <166110937+23DP2MTatu@users.noreply.github.com>
2025-02-26 09:48:48 +0000

    Money (3 part) - Part 1: Plus

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,16 +2,20 @@ package lv.rvt;
 
 public class App {
     public static void main(String[] args) {
-            Box box = new Box(10);
-        
-            box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
-            box.add(new Book("Robert Martin", "Clean Code", 1));
-            box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
-        
-            box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
-            box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
-            box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
-        
-            System.out.println(box);
+        Money a = new Money(10,0);
+        Money b = new Money(5,0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
+
+        a = a.plus(c);          // NB: a new Money object is created, and is placed "at the end of the strand connected to a"
+        //  the old 10 euros at the end of the strand disappears and the Java garbage collector takes care of it
+
+        System.out.println(a);  // 25.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
     }
 }
---
+++ src/main/java/lv/rvt/Money.java
@@ -0,0 +1,34 @@
+package lv.rvt;
+
+public class Money {
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+
+    public Money plus(Money addition) {
+        Money newMoney = new Money(addition.euros() + euros(),addition.cents() + cents()); // create a new Money object that has the correct worth
+        // return the new Money object
+        return newMoney;
+    }
+}
